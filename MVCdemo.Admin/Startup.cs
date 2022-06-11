f1f7513edc83e1e6aad506f90c596210d115ba74
redgrandfa <83260277+redgrandfa@users.noreply.github.com>
2022-06-11 13:19:31 +0800

    方案架構 + 分層 + 資料庫連線設定

--- MVCdemo.Admin/Startup.cs
+++ MVCdemo.Admin/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MVCdemo.Front
+namespace MVCdemo.Admin
 {
     public class Startup
     {
@@ -31,6 +30,7 @@ namespace MVCdemo.Front
             {
                 options.UseSqlServer(Configuration.GetConnectionString("XXX"));
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
